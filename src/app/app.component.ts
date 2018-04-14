d667e162e0330e21314dd9d56c8a604832a5d7e6
Stefan Todorinski <stefan.todorinski@adessode>
2018-04-15 01:15:45 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
+import { Component, ViewChild } from '@angular/core'
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, FormControl } from '@angular/forms';
+import { MatSnackBar, MatCheckboxChange } from '@angular/material';
+import { MatStepper } from '@angular/material';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { RequestOptions } from '@angular/http'
 
@@ -12,6 +13,8 @@ import { RequestOptions } from '@angular/http'
 export class AppComponent {
   title = 'app'
 
+  @ViewChild('progressBar') progressBar: any;
+
   isLinear = true
   credentialForm: FormGroup
   voteForm: FormGroup
@@ -19,10 +22,17 @@ export class AppComponent {
   code
   selected
   accepted
+  preferenced = 'EAT_AND_DRINK'
+  
 
   show = false
 
-  selectOptions = ["I'll come", "Sorry, i can't"]
+  selectOptions = ["Да", "Не знам, по-скоро не."]
+  preferenceOptions = [
+    { value: 'EAT_AND_DRINK', viewValue: "Някъде да ядем и пием :)"},
+    { value: 'DISCO', viewValue: "Опция 1) и mоже диско?"},
+    { value: 'I_DONT_CARE', viewValue: "? ? ? se mi e taa"}
+  ]
 
   LOCAL_URL: string = "http://localhost:8080/"
 
@@ -35,8 +45,9 @@ export class AppComponent {
       'credentialCtrl': ['', Validators.pattern('([A-Z][0-9]){2}')]
     })
     this.voteForm = this._formBuilder.group({
-      'voteCtrl': ['', Validators.required]
-    })
+      'voteCtrl': new FormControl(null, [Validators.required]),
+      'preferenceCtrl': new FormControl(null, [])
+    }, { validator: this.validator })
   }
 
   openSnackBarOnCredentials() {
@@ -51,29 +62,47 @@ export class AppComponent {
 
   openSnackBarOnVote() {
     let voteValid = this.voteForm.get('voteCtrl').valid
-    if (voteValid) {
+    let preferenceValid = this.voteForm.get('preferenceCtrl').valid
+    if ((this.selected == 0 && preferenceValid) || this.selected == 1) {
       this._snackBar.open('Successful', '', { duration: 1000 })
       this.accepted = this.selected == 0
       this.showResult(this.code, this.accepted)
+    } else if (voteValid == false){
+      this._snackBar.open('Моля изберете опция', '', { duration: 1000 })
     } else {
-      this._snackBar.open('Please select an option', '', { duration: 1000 })
+      this._snackBar.open('Моля изберете опция', '', { duration: 1000 })
     }
   }
 
   showResult(code: string, selected: number) {
-    console.info(this.code, this.accepted)
+    this.show = true
 
     let body = {
       sender: this.code,
-      accepted: this.accepted
+      accepted: this.accepted,
+      preferences: this.accepted ? [this.preferenced] : []
     }
 
     this.http
       .post(this.REMOTE_URL + "count", body)
       .subscribe(data => {
         console.info(data)
-        this.show = true
+        this.show = false
       })
   }
 
+  toggleSelect(event: MatCheckboxChange, i) {
+    this.selected = event.checked ? i : undefined
+    console.info(this.selected, this.preferenced)
+  }
+
+  togglePreference(event: MatCheckboxChange, i) {
+    this.preferenced = i
+    console.info("hehe")
+  }
+
+  validator(group: FormGroup) {
+      const forbidden = (group.controls['preferenceCtrl'].value == undefined) && (group.controls['voteCtrl'].value == 0)
+      return forbidden ? {'error': true} : null;
+    }
 }
