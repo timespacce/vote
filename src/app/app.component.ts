e5b3f58b5f9aeb963e5a51d29b6298f9758b1fc9
James Speeth <spaccetime@gmail.com>
2022-04-23 22:23:32 +0200

    - update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ import {DatePipe} from "@angular/common";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
